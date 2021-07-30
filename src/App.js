2cf99017cc223e03bb0b35c399c079fa208a6602
Sweets Sweetman <sweetmantech@gmail.com>
2021-07-30 18:14:09 +0700

    I add the IPFS & PolygonScan url to Noisy Day 5 Art Piece

--- src/App.js
+++ src/App.js
@@ -11,11 +11,19 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://gateway.pinata.cloud/ipfs/QmabqQ9S3XwubugA4rRq44UrJBL1sWuCXqVfsNiFAUYgs9"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View on IPFS
+        </a>
+        <a
+          className="App-link"
+          href="https://polygonscan.com/tx/0x8b1920d332fdf95edd859b96aa3f25ba4867ae6bebe46ead34e699bddd5842c7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View NFT on PolygonScan
         </a>
       </header>
     </div>
