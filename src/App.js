5ee3ff1442817b1636c6b2b7f48c86356adba9bd
Sweets Sweetman <sweetmantech@gmail.com>
2021-08-15 15:35:28 +0700

    I init Cat script

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import './App.css';
 import Day5 from './sketches/Day5';
+import Cat from './sketches/Cat';
 
 function App() {
+  const project = window.location.pathname;
   return (
     <div className="App">
       <header className="App-header">
-        <Day5 />        
-        <p>
-          Noisy Day 5
-        </p>
-        <a
-          className="App-link"
-          href="https://gateway.pinata.cloud/ipfs/QmabqQ9S3XwubugA4rRq44UrJBL1sWuCXqVfsNiFAUYgs9"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View on IPFS
-        </a>
-        <a
-          className="App-link"
-          href="https://polygonscan.com/tx/0x8b1920d332fdf95edd859b96aa3f25ba4867ae6bebe46ead34e699bddd5842c7"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View NFT on PolygonScan
-        </a>
+        { 
+          project.includes("cat") ? 
+          <Cat /> :
+          <Day5 />   
+        } 
       </header>
     </div>
   );
--- src/sketches/Day5.js
+++ src/sketches/Day5.js
@@ -20,82 +20,109 @@ const Day5 = (props) => {
 		// (without that p5 will render the canvas outside of your component)
 		p5.createCanvas(canvasWidth, canvasHeight).parent(canvasParentRef);
 
-        p5.mouseX = canvasWidth / 2;
-        p5.mouseY = canvasHeight / 2;
+    p5.mouseX = canvasWidth / 2;
+    p5.mouseY = canvasHeight / 2;
 	};
 
 	const draw = (p5) => {
 		p5.background(0);
   
-        const frequency = p5.lerp(minFrequency, maxFrequency, p5.mouseX / canvasWidth);
-        const amplitude = p5.lerp(minAmplitude, maxAmplitude, p5.mouseY / canvasHeight);
-        
-        const dim = Math.min(canvasWidth, canvasHeight);
-        
-        // Draw the background
-        p5.noFill();
-        p5.stroke(255);
-        p5.strokeWeight(dim * 0.0075);
-        
-        const time = p5.millis() / 1000;
-        const rows = 10;
+    const frequency = p5.lerp(minFrequency, maxFrequency, p5.mouseX / canvasWidth);
+    const amplitude = p5.lerp(minAmplitude, maxAmplitude, p5.mouseY / canvasHeight);
+    
+    const dim = Math.min(canvasWidth, canvasHeight);
+    
+    // Draw the background
+    p5.noFill();
+    p5.stroke(255);
+    p5.strokeWeight(dim * 0.0075);
+    
+    const time = p5.millis() / 1000;
+    const rows = 10;
 
-        // Draw each line
-        for (let y = 0; y < rows; y++) {
-            // Determine the Y position of the line
-            const v = rows <= 1 ? 0.5 : y / (rows - 1);
-            const py = v * canvasHeight;
-            drawNoiseLine({
-                v,
-                start: [ 0, py ],
-                end: [ canvasWidth, py ],
-                amplitude: amplitude * canvasHeight,
-                frequency,
-                time: time * 0.5,
-                steps: 150,
-                p5
-            });
-        }
+    // Draw each line
+    for (let y = 0; y < rows; y++) {
+        // Determine the Y position of the line
+        const v = rows <= 1 ? 0.5 : y / (rows - 1);
+        const py = v * canvasHeight;
+        drawNoiseLine({
+            v,
+            start: [ 0, py ],
+            end: [ canvasWidth, py ],
+            amplitude: amplitude * canvasHeight,
+            frequency,
+            time: time * 0.5,
+            steps: 150,
+            p5
+        });
+    }
 	};
 
-    const drawNoiseLine = (opt = {}) => {
-        const {
-          v,
-          start,
-          end,
-          steps = 10,
-          frequency = 1,
-          time = 0,
-          amplitude = 1,
-          p5
-        } = opt;
+  const drawNoiseLine = (opt = {}) => {
+      const {
+        v,
+        start,
+        end,
+        steps = 10,
+        frequency = 1,
+        time = 0,
+        amplitude = 1,
+        p5
+      } = opt;
+      
+      const [ xStart, yStart ] = start;
+      const [ xEnd, yEnd ] = end;
+    
+      // Create a line by walking N steps and interpolating
+      // from start to end point at each interval
+      p5.beginShape();
+      for (let i = 0; i < steps; i++) {
+        // Get interpolation factor between 0..1
+        const t = steps <= 1 ? 0.5 : i / (steps - 1);
+    
+        // Interpolate X position
+        const x = p5.lerp(xStart, xEnd, t);
         
-        const [ xStart, yStart ] = start;
-        const [ xEnd, yEnd ] = end;
-      
-        // Create a line by walking N steps and interpolating
-        // from start to end point at each interval
-        p5.beginShape();
-        for (let i = 0; i < steps; i++) {
-          // Get interpolation factor between 0..1
-          const t = steps <= 1 ? 0.5 : i / (steps - 1);
-      
-          // Interpolate X position
-          const x = p5.lerp(xStart, xEnd, t);
-          
-          // Interpolate Y position
-          let y = p5.lerp(yStart, yEnd, t);
-          
-          // Offset Y position by noise
-          y += (simplex.noise3D(t * frequency + time, v * frequency, time)) * amplitude;
-          
-          // Place vertex
-          p5.vertex(x, y);
-        }
-        p5.endShape();
+        // Interpolate Y position
+        let y = p5.lerp(yStart, yEnd, t);
+        
+        // Offset Y position by noise
+        y += (simplex.noise3D(t * frequency + time, v * frequency, time)) * amplitude;
+        
+        // Place vertex
+        p5.vertex(x, y);
       }
+      p5.endShape();
+    }
 
-	return <Sketch setup={setup} draw={draw} />;
+	return (
+    <div>
+      <Sketch setup={setup} draw={draw} />
+      <p>
+        Noisy Day 5
+      </p>
+      <p>
+        <a
+          className="App-link"
+          href="https://gateway.pinata.cloud/ipfs/QmabqQ9S3XwubugA4rRq44UrJBL1sWuCXqVfsNiFAUYgs9"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View on IPFS
+        </a>
+      </p>
+      <p>
+        <a
+          className="App-link"
+          href="https://polygonscan.com/tx/0x8b1920d332fdf95edd859b96aa3f25ba4867ae6bebe46ead34e699bddd5842c7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View NFT on PolygonScan
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Day5;
